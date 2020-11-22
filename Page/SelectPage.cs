5abed18c3bca919ed5888fd3fd60874ee9ba06dd
Yegor Levandovskyi <levandovskyi98@gmail.com>
2020-11-23 01:17:55 +0200

    first commit

---
+++ Page/SelectPage.cs
@@ -0,0 +1,50 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Interactions;
+using OpenQA.Selenium.Support.UI;
+using System.Linq;
+
+namespace HW4.Page
+{
+    class SelectPage
+    {
+        private IWebDriver _driver;
+        public IWebElement dropdownElement => _driver.FindElement(By.Id("multi-select"));
+        public SelectElement dropdown => new SelectElement(dropdownElement);
+        public IWebElement FirstSelectedBtn => _driver.FindElement(By.Id("printMe"));
+        public IWebElement AllSelectedBtn => _driver.FindElement(By.Id("printAll"));
+        public IWebElement AllSelectedElement => _driver.FindElement(By.ClassName("getall-selected"));
+        public SelectPage(IWebDriver driver) { _driver = driver; }
+
+        public SelectPage SelectOptions(int[] ids)
+        {
+            dropdown.DeselectAll();
+            Actions actions = new Actions(_driver);
+            actions.KeyDown(Keys.Control);
+            foreach (int id in ids)
+            {
+                if (id < dropdown.Options.Count - 1)
+                    actions.Click(dropdown.Options[id]);
+            }
+            actions.KeyUp(Keys.Control);
+            actions.Build().Perform();
+            return this;
+        }
+
+        public SelectPage ClickFirstSelected()
+        {
+            FirstSelectedBtn.Click();
+            return this;
+        }
+        public SelectPage ClickAllSelected()
+        {
+            AllSelectedBtn.Click();
+            return this;
+        }
+
+        public string[] GetSelectedArray()
+        {
+            return dropdown.AllSelectedOptions.Select(option => option.Text).ToArray();
+        }
+
+    }
+}
---
+++ Test/SelectTest.cs
@@ -0,0 +1,57 @@
+﻿using HW4.Page;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Linq;
+
+namespace HW4.Test
+{
+    class SelectTest
+    {
+        private IWebDriver _driver;
+        [OneTimeSetUp]
+        public void SetUp()
+        {
+            _driver = new ChromeDriver();
+            _driver.Navigate().GoToUrl("https://www.seleniumeasy.com/test/basic-select-dropdown-demo.html");
+            _driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
+            _driver.Manage().Window.Maximize();
+        }
+
+        [TestCase]
+        public void TestTwo()
+        {
+            SelectPage page = new SelectPage(_driver).SelectOptions(new int[] { 0, 1 }).ClickFirstSelected();
+            string firstValue = page.dropdown.AllSelectedOptions[0].GetAttribute("value");
+            Assert.IsTrue(page.AllSelectedElement.Text.Contains(firstValue), "Shows first selected state");
+        }   
+        [TestCase]
+        public void TestThree()
+        {
+            SelectPage page = new SelectPage(_driver).SelectOptions(new int[] { 2, 1, 3 }).ClickFirstSelected();
+            string firstValue = page.dropdown.AllSelectedOptions[2].GetAttribute("value");
+            Assert.IsTrue(page.AllSelectedElement.Text.Contains(firstValue), "Shows first selected state");
+        }        
+        [TestCase]
+        public void TestAllSelectedForTwo()
+        {
+            SelectPage page = new SelectPage(_driver).SelectOptions(new int[] { 3, 0 }).ClickAllSelected();
+            bool intersects = page.GetSelectedArray().Intersect(page.AllSelectedElement.Text.Split(" ")).Any();
+            Assert.IsTrue(intersects);
+        }       
+        [TestCase]
+        public void TestAllSelectedForFour()
+        {
+            SelectPage page = new SelectPage(_driver).SelectOptions(new int[] { 2, 1, 3, 0 }).ClickAllSelected();
+            bool intersects = page.GetSelectedArray().Intersect(page.AllSelectedElement.Text.Split(" ")).Any();
+            Assert.IsTrue(intersects);
+        }
+
+        [OneTimeTearDown]
+        public void TearDown()
+        {
+            _driver.Quit();
+        }
+    }
+}
